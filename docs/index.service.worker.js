d1d0b7915c9bd071c1e8c560f130fca03214ba47
KOPP <koppkvn@gmail.com>
2026-01-25 12:53:25 +0100

    x

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1769334559|1820508120';
+const CACHE_VERSION = '1769341997|10628335';
 /** @type {string} */
 const CACHE_PREFIX = 'Iso Tactics-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
